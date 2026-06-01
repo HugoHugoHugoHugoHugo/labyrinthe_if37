90bdb30facdc39b15050a7aada0e216fb4da23ba
Gabriel <gabriel.ewenczyk@utt.fr>
2026-06-01 19:06:02 +0200

    Ajout du mouvement du joueur
    Mouvement avec State Machine qui diffère entre le ActionState et le ExplorationState

--- Assets/Player/PlayerStateMachine.cs
+++ Assets/Player/PlayerStateMachine.cs
@@ -1,16 +1,55 @@
+using OperatorOverload.Bridge.Serialization;
 using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.DualShock;
 
 public class PlayerStateMachine : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    public GameObject Terrain;
+    public Camera Camera;
+
+    public float CameraRotationSpeed = 10f;
+
+    public PlayerState explorationState = new ExplorationPlayerState();
+    public PlayerState actionState = new ActionPlayerState();
+
+    public Gamepad gamepad;
+
+    private PlayerState _state = null;
+
     void Start()
     {
-        
+        GetComponent<Rigidbody>().sleepThreshold = 0f;
+        SetState(actionState);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        HandleCamera();
+
+        PlayerState newState = _state.Update(this, Time.deltaTime);
+        if (newState != null)
+        {
+            SetState(newState);
+        }
+    }
+
+    private void SetState(PlayerState newState)
+    {
+        if (_state != null)
+            _state.Exit(this);
+        _state = newState;
+        _state.Enter(this);
+    }
+
+    private void HandleCamera()
+    {
+        Camera.transform.position = transform.position - Camera.transform.forward * 4f;
+
+        Vector3 gamepadRotation = new Vector3(0, Gamepad.current.rightStick.ReadValue().x, 0);
+        Camera.transform.rotation = Quaternion.Euler(Camera.transform.rotation.eulerAngles + gamepadRotation * CameraRotationSpeed * Time.deltaTime);
+
+        transform.GetChild(0).transform.rotation = Camera.transform.rotation; // Pour le son
     }
 }
