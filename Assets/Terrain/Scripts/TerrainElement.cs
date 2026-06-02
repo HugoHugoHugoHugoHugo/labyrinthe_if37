832d25167a77a49df2cba958f83768a6557644ac
HugoHugoHugoHugoHugo <hhugo53810@gmail.com>
2026-06-02 13:31:50 +0200

    Added : audio sources now move within the terrain element so as to always be as close to the player as possible

--- Assets/Terrain/Scripts/TerrainElement.cs
+++ Assets/Terrain/Scripts/TerrainElement.cs
@@ -1,3 +1,6 @@
+using NUnit.Framework.Internal;
+using Unity.Mathematics;
+using Unity.Mathematics.Geometry;
 using UnityEngine;
 
 public class TerrainElement : MonoBehaviour
@@ -9,11 +12,30 @@ public class TerrainElement : MonoBehaviour
     //Played continuously when touched or not by player
     public AudioClip ContinuousSound;
 
-    private AudioSource m_source;
+    public AudioSource Source;
+    private Vector3 m_boundsSize;
 
     void Start()
     {
-        m_source = GetComponentInChildren<AudioSource>();
+        m_boundsSize = GetComponent<Renderer>().bounds.size;
+    }
+
+    void Update()
+    {
+        Vector3 playerPos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        Vector3 objectPos = transform.position;
+        //Gives the position of the player from the object's origin (center of the object)
+        Vector3 playerDirection = playerPos - objectPos;
+        //Transform the vector representing the object's dimensions from local scale to global scale
+        Vector3 globalBoundSize = transform.TransformDirection(m_boundsSize);
+        //Make sure the new position is within bounds of the object
+        Vector3 newPos = new Vector3(Mathf.Sign(playerDirection.x)*Mathf.Min(Mathf.Abs(playerDirection.x), globalBoundSize.x*.5f), 
+            Mathf.Sign(playerDirection.y)*Mathf.Min(Mathf.Abs(playerDirection.y), globalBoundSize.y*.5f),
+            Mathf.Sign(playerDirection.z)*Mathf.Min(Mathf.Abs(playerDirection.z), globalBoundSize.z*.5f));
+        //Move the audio source to the new position
+        Source.transform.position = transform.position + newPos;
+           
+        
     }
     void OnCollisionEnter(Collision collision)
     {
@@ -23,12 +45,12 @@ public class TerrainElement : MonoBehaviour
             switch (Caracteristics.Type)
             {
                 case TerrainElementCaracteristics.ElementType.MUR :
-                    m_source.PlayOneShot(OnContactSound);
-                    m_source.Play();
+                    Source.PlayOneShot(OnContactSound);
+                    Source.Play();
                     Debug.Log("Hit a wall");
                     break;
                 case TerrainElementCaracteristics.ElementType.DRAPEAU :
-                    m_source.PlayOneShot(OnContactSound);
+                    Source.PlayOneShot(OnContactSound);
                     Debug.Log("Hit the win condition !");
                     break;
                 default :
@@ -56,7 +78,7 @@ public class TerrainElement : MonoBehaviour
         {
             if (Caracteristics.Type == TerrainElementCaracteristics.ElementType.MUR)
             {
-                m_source.Stop();
+                Source.Stop();
                 Debug.Log("Left a wall");
             }
             
