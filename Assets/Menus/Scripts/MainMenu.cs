7daf1cde7fd05c26408dbd128c08b25367c1eb5d
HugoHugoHugoHugoHugo <hhugo53810@gmail.com>
2026-06-11 18:52:59 +0200

    Added : final sounds for Main and Level Menus

--- Assets/Menus/Scripts/MainMenu.cs
+++ Assets/Menus/Scripts/MainMenu.cs
@@ -1,14 +1,29 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using static Unity.VisualScripting.Member;
 
 public class MainMenu : MonoBehaviour
 {
+    static private bool m_firstAccess = true;
     public GameObject FirstSelected;
     private GameObject m_currentSelectedButton;
+    private AudioSource m_audioSource;
     void Start()
     {
+        StartCoroutine(Initiate());
+    }
+    IEnumerator Initiate()
+    {
+        m_audioSource = GetComponent<AudioSource>();
+        if (m_firstAccess)
+        {
+            m_audioSource.Play();
+            yield return new WaitWhile(() => m_audioSource.isPlaying);
+            m_firstAccess = false;
+        }
         //Censé être géré par EventSystem directement mais ne marche pas, donc besoin de la ligne suivante
         EventSystem.current.SetSelectedGameObject(FirstSelected);
         m_currentSelectedButton = EventSystem.current.currentSelectedGameObject;
